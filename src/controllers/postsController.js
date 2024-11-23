18a92f57aa42ea2b2300ed3b736ec4980ed9f08e
imjowend <joaquinvcestudio@gmail.com>
2024-11-23 08:43:33 -0500

    Feat: Primer upload, mitad clase 4

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,28 @@
-import { getTodosPosts } from "../models/postsModel.js";
+import { getTodosPosts, criarPost } from "../models/postsModel.js";
 
 export async function listarPosts(req, res) {
   const posts = await getTodosPosts();
     res.status(200).json(posts);
+}
+
+export async function postarNovoPost(req, res) {
+  const novoPost = req.body;
+  try {
+    const postCriado = await criarPost(novoPost);
+    res.status(200).json(postCriado);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({"Error":"Falha na requisicao"})
+  }
+}
+
+export async function uploadImagem(req, res) {
+  const novoPost = req.body;
+  try {
+    const postCriado = await criarPost(novoPost);
+    res.status(200).json(postCriado);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({"Error":"Falha na requisicao"})
+  }
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,26 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+import multer from "multer";
+/*
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})*/
+
+//const upload = multer({ dest: "./uploads", storage})
+const upload = multer({ dest: "./uploads"})
 
 const routes = (app) => {
   app.use(express.json());
 
   app.get("/posts", listarPosts);
+
+  app.post("/posts", postarNovoPost);
+  app.post("/upload", upload.single("imagem"), uploadImagem);
 }
 
 export default routes;
